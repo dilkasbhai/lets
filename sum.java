9d0710aed1547ab4fe90ddb8bbd230ca4fe73cc5
dilkashbhai <adsdigitalnepal@gmail.com>
2025-05-24 15:55:33 +0545

    initail commit

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class SumExample {
+    public static void main(String[] args) {
+        int num1 = 10;  // First number
+        int num2 = 20;  // Second number
+
+        int sum = num1 + num2;  // Calculating sum
+
+        System.out.println("The sum is: " + sum);
+    }
+}
