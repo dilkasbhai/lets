b67be6d2f97adb2bc33b8c8a26aaf7cd8efe3190
dilkashbhai <adsdigitalnepal@gmail.com>
2025-05-24 16:05:09 +0545

    msg

--- sum.java
+++ sum.java
@@ -1,7 +1,7 @@
 public class SumExample {
     public static void main(String[] args) {
         int num1 = 10;  // First number
-        int num2 = 20;  // Second number
+        int num2 = 10;   // Second number
 
         int sum = num1 + num2;  // Calculating sum
 
